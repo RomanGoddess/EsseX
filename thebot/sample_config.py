c77792f5fa4ae363187cce955b888e894fb44471
女神 <53878781+RomanGoddess@users.noreply.github.com>
2020-12-18 16:17:40 +0100

    Update sample_config.py

--- thebot/sample_config.py
+++ thebot/sample_config.py
@@ -1,11 +1,10 @@
 class Config(object):
-    LOGGER = True
-    API_ID = 69
-    API_HASH = "owo"
-    TOKEN = "uwu"
-    DB_URI = ""
-    LOG_CHANNEL = 0 # if you want a logging channel you can add this, else logs will go into Owner's PM
+   LOGGER = True
+   API_ID = 1645928
+   API_HASH = "6874b8b3d4aef43cd79cc32d0fc86af8"
+   TOKEN = "1405317749:AAFejGg60ACk2qbH7bDTwTNuySAXgy5_jhU"
+   DB_URI = "postgres://mrbcrvit:m3juoeaXEgJMOeUXhLqikEQaIA1vmfpb@satao.db.elephantsql.com:5432/mrbcrvit"
 
 class Development(Config):
-    TEST_DEVELOP = True
-    LOGGER = True
+   TEST_DEVELOP = True
+   LOGGER = True
